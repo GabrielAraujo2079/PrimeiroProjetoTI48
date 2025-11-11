52a9ab66055254943cca0542986826fa4c9b5ac6
Gabriel The little Black <Gabrielaraujo20079@gmail.com>
2025-11-11 17:15:09 -0300

    Calculadora Mais forte

--- PrimeiroProjetoTI48/Form1.cs
+++ PrimeiroProjetoTI48/Form1.cs
@@ -29,16 +29,26 @@ namespace PrimeiroProjetoTI48
         private void Subtracao_Click(object sender, EventArgs e)
         {
 
+            double soma1 = double.Parse(n1.Text);
+            double soma2 = double.Parse(n2.Text);
+            double resultado = soma1 - soma2;
+            n3.Text = resultado.ToString();
         }
 
         private void Mult_Click(object sender, EventArgs e)
         {
-
+            double soma1 = double.Parse(n1.Text);
+            double soma2 = double.Parse(n2.Text);
+            double resultado = soma1 * soma2;
+            n3.Text = resultado.ToString();
         }
 
         private void Divisao_Click(object sender, EventArgs e)
         {
-
+            double soma1 = double.Parse(n1.Text);
+            double soma2 = double.Parse(n2.Text);
+            double resultado = soma1 / soma2;
+            n3.Text = resultado.ToString();
         }
     }
 }
